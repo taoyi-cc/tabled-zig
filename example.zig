f6f271c77167cbd759906cebea76475155cfc4df
cc <ceramics0303@163.com>
2026-05-13 20:41:54 +0800

    Modern style

--- example.zig
+++ example.zig
@@ -4,20 +4,69 @@ const Table = tabled.Table;
 
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
-    // _ = gpa;
-    const data1: [3][]const []const u8 = .{
-        &[_][]const u8{ "aadd", "v", "c", "d", "hahah" },
-        &[_][]const u8{ "aa", "v", "c", "d" },
-        &[_][]const u8{ "v", "c", "d" },
-    };
-    const data2: [3][]const []const u8 = .{
-        &[_][]const u8{ "Col1 ", "Col2", "Col3", "Col4", "Col5" },
-        &[_][]const u8{ "aa", "v", "c", "d" },
-        &[_][]const u8{ "v", "c", "d" },
-    };
+    const io = init.io;
 
-    var table = Table.init(gpa, .{ .style = .round });
-    defer table.deinit();
-    std.debug.print("{s}\n", .{try table.build(&data1)});
-    std.debug.print("{s}\n", .{try table.build(&data2)});
+    var list = try readDir(gpa, io);
+    defer {
+        for (list.items) |*it| {
+            it.deinit();
+        }
+        list.deinit(gpa);
+    }
+
+    const file_data = try gpa.alloc([]const []const u8, list.items.len + 1);
+    defer {
+        for (0..file_data.len) |i| {
+            if (i > 0) {
+                const line = file_data[i];
+                gpa.free(line);
+            }
+        }
+        gpa.free(file_data);
+    }
+    file_data[0] = &.{ "Name", "Size(Byte)", "Type" };
+    var buf: [64]u8 = undefined;
+    for (list.items, 0..) |it, i| {
+        const line = try gpa.alloc([]const u8, 3);
+        line[0] = it.name;
+        line[1] = try std.fmt.bufPrint(&buf, "{}", .{it.size});
+        line[2] = it.type;
+        file_data[i + 1] = line;
+    }
+
+    var t = Table.init(gpa, .{ .style = .ascii });
+    defer t.deinit();
+    std.debug.print("{s}\n", .{try t.build(file_data)});
+}
+
+const FileMeta = struct {
+    allocator: std.mem.Allocator,
+    name: []const u8,
+    size: usize,
+    type: []const u8,
+
+    fn init(gpa: std.mem.Allocator, name: []const u8, stat: std.Io.File.Stat) !FileMeta {
+        return .{
+            .allocator = gpa,
+            .name = try gpa.dupe(u8, name),
+            .size = stat.size,
+            .type = if (stat.kind == .file) "file" else "dir",
+        };
+    }
+
+    fn deinit(self: *FileMeta) void {
+        self.allocator.free(self.name);
+    }
+};
+
+fn readDir(gpa: std.mem.Allocator, io: std.Io) !std.ArrayList(FileMeta) {
+    const cwd = try std.Io.Dir.cwd().openDir(io, ".", .{ .iterate = true });
+    defer cwd.close(io);
+    var iter = cwd.iterate();
+    var list = std.ArrayList(FileMeta).empty;
+    while (try iter.next(io)) |entry| {
+        const stat = try cwd.statFile(io, entry.name, .{});
+        try list.append(gpa, try FileMeta.init(gpa, entry.name, stat));
+    }
+    return list;
 }
--- src/root.zig
+++ src/root.zig
@@ -1,40 +1,71 @@
 const std = @import("std");
 
+pub const Style = enum { none, ascii, ascii_round, modern };
+
 pub const Border = struct {
-    gadget: u8,
-    top_left: u8,
-    top_right: u8,
-    bottom_left: u8,
-    bottom_right: u8,
+    gadget: []const u8,
+    top_gadget: []const u8,
+    bottom_gadget: []const u8,
+    left_gadget: []const u8,
+    right_gadget: []const u8,
+    top_left: []const u8,
+    top_right: []const u8,
+    bottom_left: []const u8,
+    bottom_right: []const u8,
     line_delimiter: []const u8,
     col_delimiter: []const u8,
 
     fn build(style: Style) Border {
         switch (style) {
-            .line => return .{
-                .gadget = '+',
-                .top_left = '+',
-                .top_right = '+',
-                .bottom_left = '+',
-                .bottom_right = '+',
+            .ascii => return .{
+                .gadget = "+",
+                .top_gadget = "+",
+                .bottom_gadget = "+",
+                .left_gadget = "+",
+                .right_gadget = "+",
+                .top_left = "+",
+                .top_right = "+",
+                .bottom_left = "+",
+                .bottom_right = "+",
                 .line_delimiter = "-",
                 .col_delimiter = "|",
             },
-            .round => return .{
-                .gadget = '-',
-                .top_left = '.',
-                .top_right = '.',
-                .bottom_left = '\'',
-                .bottom_right = '\'',
+            .ascii_round => return .{
+                .gadget = "-",
+                .top_gadget = "-",
+                .bottom_gadget = "-",
+                .left_gadget = "-",
+                .right_gadget = "-",
+                .top_left = ".",
+                .top_right = ".",
+                .bottom_left = "'",
+                .bottom_right = "'",
                 .line_delimiter = "-",
                 .col_delimiter = "|",
+            },
+            .modern => return .{
+                .gadget = "┼",
+                .top_gadget = "┬",
+                .bottom_gadget = "┴",
+                .left_gadget = "├",
+                .right_gadget = "┤",
+                .top_left = "╭",
+                .top_right = "╮",
+                .bottom_left = "╰",
+                .bottom_right = "╯",
+                .line_delimiter = "─",
+                .col_delimiter = "│",
             },
             .none => return .{
-                .gadget = ' ',
-                .top_left = ' ',
-                .top_right = ' ',
-                .bottom_left = ' ',
-                .bottom_right = ' ',
+                .gadget = "",
+                .top_gadget = "",
+                .bottom_gadget = "",
+                .left_gadget = "",
+                .right_gadget = "",
+                .top_left = "",
+                .top_right = "",
+                .bottom_left = "",
+                .bottom_right = "",
                 .line_delimiter = "",
                 .col_delimiter = "",
             },
@@ -42,15 +73,8 @@ pub const Border = struct {
     }
 };
 
-pub const Style = enum { none, line, round };
-pub const Info = struct {
-    line_delimiter: []const u8,
-    col_delimiter: []const u8,
-    corner: u8,
-    gadget: u8,
-};
 pub const Config = struct {
-    style: Style = .line,
+    style: Style = .ascii,
 };
 
 pub const Table = struct {
@@ -80,26 +104,20 @@ pub const Table = struct {
         }
         const border = self.border;
         const gpa = self.allocator;
-        for (0..col_count) |i| {
-            const len = col_lengths[i];
-            if (i == 0) {
-                try self.list.append(gpa, if (line_no == 0) border.top_left else if (line_no == data.len) border.bottom_left else border.gadget);
-            } else if (i == col_count) {
-                try self.list.append(gpa, if (line_no == 0) border.top_right else if (line_no == data.len) border.bottom_right else border.gadget);
+        for (0..col_count + 1) |i| {
+            if (line_no == 0) {
+                try self.list.appendSlice(gpa, if (i == 0) border.top_left else if (i == col_count) border.top_right else border.top_gadget);
+            } else if (line_no == data.len) {
+                try self.list.appendSlice(gpa, if (i == 0) border.bottom_left else if (i == col_count) border.bottom_right else border.bottom_gadget);
             } else {
-                try self.list.append(gpa, border.gadget);
+                try self.list.appendSlice(gpa, if (i == 0) border.left_gadget else if (i == col_count) border.right_gadget else border.gadget);
             }
-            for (0..len + gap * 2) |_| {
-                try self.list.appendSlice(gpa, border.line_delimiter);
+            if (i < col_lengths.len) {
+                const len = col_lengths[i];
+                for (0..len + gap * 2) |_| {
+                    try self.list.appendSlice(gpa, border.line_delimiter);
+                }
             }
-        }
-        // std.debug.print("{}", .{data.len});
-        if (line_no == 0) {
-            try self.list.append(gpa, border.top_right);
-        } else if (line_no == data.len) {
-            try self.list.append(gpa, border.bottom_right);
-        } else {
-            try self.list.append(gpa, border.gadget);
         }
         try self.list.append(gpa, '\n');
     }
@@ -107,7 +125,6 @@ pub const Table = struct {
     pub fn build(self: *Table, data: []const []const []const u8) ![]u8 {
         const gpa = self.allocator;
         self.list.clearRetainingCapacity();
-        // const border = Border.build(self.config.style);
         const gap = 1;
         const col_count = blk: {
             var max: usize = 0;
@@ -128,20 +145,6 @@ pub const Table = struct {
         };
         defer gpa.free(col_lengths);
 
-        // std.debug.print("{}\n", .{line_len});
-
-        // if (self.config.style != .none) {
-        //     for (0..col_count) |i| {
-        //         const len = col_lengths[i];
-        //         try self.list.append(gpa, if (i == 0) border.top_left else if (i == col_count) border.top_right else border.gadget);
-        //         for (0..len + gap * 2) |_| {
-        //             try self.list.appendSlice(gpa, border.line_delimiter);
-        //         }
-        //     }
-
-        //     try self.list.append(gpa, border.top_right);
-        //     try self.list.append(gpa, '\n');
-        // }
         try self.buildLineDelimiter(data, gap, col_count, col_lengths, 0);
 
         for (data, 0..) |line, line_no| {
@@ -163,31 +166,6 @@ pub const Table = struct {
             try self.list.append(gpa, '\n');
 
             try self.buildLineDelimiter(data, gap, col_count, col_lengths, line_no + 1);
-            // if (self.config.style != .none) {
-            //     for (0..col_count) |i| {
-            //         std.debug.print("{any}\n", .{line_no == 0});
-            //         const len = col_lengths[i];
-            //         if (line_no == 0) {
-            //             try self.list.append(gpa, if (i == 0) border.top_left else if (i == col_count - 1) border.top_right else border.gadget);
-            //         } else if (line_no == data.len - 1) {
-            //             try self.list.append(gpa, if (i == 0) border.bottom_left else if (i == col_count - 1) border.bottom_right else border.gadget);
-            //         } else {
-            //             try self.list.append(gpa, border.gadget);
-            //         }
-            //         for (0..len + gap * 2) |_| {
-            //             try self.list.appendSlice(gpa, border.line_delimiter);
-            //         }
-            //     }
-            //     // std.debug.print("{}", .{data.len});
-            //     // if (line_no == 0) {
-            //     // try self.list.append(gpa, border.top_right);
-            //     // } else if (line_no == data.len - 1) {
-            //     // try self.list.append(gpa, border.bottom_right);
-            //     // } else {
-            //     try self.list.append(gpa, 'a');
-            //     // }
-            //     try self.list.append(gpa, '\n');
-            // }
         }
         return self.list.items;
     }
